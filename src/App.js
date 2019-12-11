e8c3f87917b6de7a746a992a8e20f6bc0fe0b3df
Briam <briammartinez@gmail.com>
2019-12-11 23:47:05 +0100

    Initial version of app file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+
+import List from './components/List/List';
+
+import styles from './App.module.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h1 className={styles.title_app}>Listado de convocatorias</h1>
+      <div className="App">
+        <List />
+      </div>
+    </Fragment>
   );
 }
 
